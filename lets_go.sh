a2470f5a3ec905694e1cb66fdef4d30d9803e1fd
alexkaiser34 <alexkaiser@me.com>
2022-04-24 15:51:57 -0400

    tracking oh my zsh dir

--- lets_go.sh
+++ lets_go.sh
@@ -2,15 +2,14 @@
 
 zsh_setup(){
     sudo apt install zsh
-    sudo -s
-    chsh -s /bin/zsh root
-    chsh -s /bin/zsh $USER
+    sudo chsh -s /bin/zsh root
+    sudo chsh -s /bin/zsh $USER
 }
 
 copy_configs(){
     sudo cp p10k.zsh ~/.p10k.zsh
     sudo cp zshrc ~/.zshrc
-    sudo cp -r oh-my-zsh ~/.oh-my-zsh
+    sudo cp -r ohmyzsh ~/.oh-my-zsh
 }
 
 install_font(){
@@ -21,11 +20,16 @@ install_font(){
 
 echo_instructions(){
     echo "Please restart VM!"
-    echo "Change terminal font to Hack after restart!"
+    echo "Change terminal font to Hack after restart in terminal preferences!"
+    echo "In terminal preferences-> colors"
+    echo "Built in schemes: Custom"
+    echo "Default Color: Text = white, Background = Black"
+    echo "Bold Color = Light purple"
+    echo "Check use transparency from system theme"
 }
 
-if [[ "$USER" != "root" ]]; then
-    echo "Error, must run this script as root!!"
+if [[ "$USER" == "root" ]]; then
+    echo "Error, must not be run this script as root!!"
     exit 1
 fi
 
